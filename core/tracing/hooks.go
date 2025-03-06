96bea45f46bb7a8a180e8f39964f7c5b13bd1147
davidpang731 <davidpang731@gmail.com>
2025-03-06 03:34:01 +0000

    feat: support write block change into kafka

--- core/tracing/hooks.go
+++ core/tracing/hooks.go
@@ -167,6 +167,9 @@ type (
 
 	// LogHook is called when a log is emitted.
 	LogHook = func(log *types.Log)
+
+	// CommitHook is called when the state is committed.
+	CommitHook = func(originRoot common.Hash, root common.Hash, destructs map[common.Hash]struct{}, accounts map[common.Hash][]byte, accountsOrigin map[common.Address][]byte, storages map[common.Hash]map[common.Hash][]byte, storagesOrigin map[common.Address]map[common.Hash][]byte, codes map[common.Hash][]byte)
 )
 
 type Hooks struct {
@@ -193,6 +196,9 @@ type Hooks struct {
 	OnCodeChange    CodeChangeHook
 	OnStorageChange StorageChangeHook
 	OnLog           LogHook
+
+	// custom hook
+	OnCommit CommitHook
 }
 
 // BalanceChangeReason is used to indicate the reason for a balance change, useful
