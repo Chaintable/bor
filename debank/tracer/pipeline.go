54b28bfaec8f43b52ea4421ec7dcd821c2bafe22
0byteme <819643718@qq.com>
2025-06-13 12:27:59 +0800

    add log

--- debank/tracer/pipeline.go
+++ debank/tracer/pipeline.go
@@ -150,6 +150,9 @@ func uploadBlockHeader(blockHeader *ptypes.Header) error {
 	if err != nil {
 		return fmt.Errorf("failed to serialize block header: %v", err)
 	}
+	if blockHeader.Number.ToInt().Uint64() == 2627299 {
+		log.Info("Upload block 2627299", "s3file", s3BlockFile, "data length", len(s3BlockFile.Data))
+	}
 	err = NodeXPusher.UploadFile(s3BlockFile)
 	if err != nil {
 		return fmt.Errorf("failed to upload block header: %v", err)
